9496938bc966a6226ef53372aef1cbaf47b64f0d
helel.zhou <helel.zhou@sandalwoodadvisors.com>
2021-11-01 23:47:53 +0800

    2021-11-01

--- 图解算法/BinarySearch/demo1.py
+++ 图解算法/BinarySearch/demo1.py
@@ -1,1 +1,19 @@
-print('hello,github!')
+from typing import List
+
+
+def binary_search(num_list: List, item):
+    low = 0
+    high = len(num_list) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if num_list[mid] > item:
+            high = mid - 1
+        elif num_list[mid] < item:
+            low = mid + 1
+        else:
+            return mid
+    return None
+
+
+nums = [1, 2, 3, 4, 5, 6, 7]
+print(binary_search(nums, 2))
